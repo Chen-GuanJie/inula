f18c05e03870dab017a4563e1dbfed2251454556
cgj <18700163154@163.com>
2024-01-03 22:41:38 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
   ],
   root: true,
 
-  plugins: ['jest', 'no-for-of-loops', 'no-function-declare-after-return', 'react', '@typescript-eslint'],
+  plugins: ['jest', 'no-for-of-loops', 'no-function-declare-after-return', 'react', '@typescript-eslint','jsdoc'],
 
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -58,6 +58,48 @@ module.exports = {
     'no-constant-condition': 'off',
     'no-for-of-loops/no-for-of-loops': 'error',
     'no-function-declare-after-return/no-function-declare-after-return': 'error',
+
+    // jsdoc注释规则
+    'jsdoc/require-jsdoc': [
+      'warn',
+      {
+        require: {
+          ArrowFunctionExpression: true,
+          ClassDeclaration: true,
+          ClassExpression: true,
+          FunctionExpression: true,
+          MethodDefinition: true
+        },
+        contexts: ['TSInterfaceDeclaration'] // 检查接口是否写注释
+      }
+    ], // 检查函数是否写注释
+    'jsdoc/check-alignment': 'warn', // 注释的*号是否对齐
+    'jsdoc/check-examples': 'warn', // 在每个函数注释中要写一个@example fun()(如何使用这个函数)
+    'jsdoc/check-indentation': 'warn', // 注释内有空格无效填充
+    'jsdoc/check-param-names': ['warn'], // 检查函数参数名称和注释是否匹配
+    'jsdoc/check-syntax': 'warn',
+    'jsdoc/check-tag-names': 'warn', // 检查标签名是否有效
+    'jsdoc/check-types': 'warn', // 报告无效类型
+    'jsdoc/implements-on-classes': 'warn', //
+    'jsdoc/match-description': [
+      'warn',
+    ], // 对描述通过正则表单式匹配
+    'jsdoc/newline-after-description': 'warn', // 在描述注释之后必须空一行
+    'jsdoc/no-types': 'warn', // 不需要写类型
+    'jsdoc/no-undefined-types': 'warn', // 写的类型不在规则里面
+    'jsdoc/require-description': ['warn', { descriptionStyle: 'any' }], // 别要求写描述
+    'jsdoc/require-description-complete-sentence': ['warn'], // 描述必须以大写开头.结尾
+    'jsdoc/require-example': 'warn', // 别要求写例子
+    'jsdoc/require-hyphen-before-param-description': 'warn', // 参数描述之前和描述之间必须-链接
+    'jsdoc/require-param': 'warn', // 别要求写参数
+    'jsdoc/require-param-description': 'warn', // 参数必须写描述
+    'jsdoc/require-param-name': 'warn', // 参数必须写名字
+    'jsdoc/require-param-type': 'warn', // 参数必须下类型jsdoc
+    'jsdoc/require-returns': 'warn', // 要求写return注释
+    'jsdoc/require-returns-check': 'warn', // 检查是否有必要写return注释
+    'jsdoc/require-returns-description': 'warn', // 要求写return藐视
+    'jsdoc/require-returns-type': 'warn', // 要求写return类型
+    'jsdoc/valid-types': 'warn', // 有效的类型
   },
   globals: {
     isDev: true,
